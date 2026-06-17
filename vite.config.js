e844179015fed7a9c99296a61c7eb93a3f66cd4d
5b2g0901 <5b2g0901@stust.edu.tw>
2026-06-17 21:26:32 +0800

    fix: blank page config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/WeatherApp/", // 🌟 就是這行！它能解決 GitHub Pages 畫面空白的問題
   plugins: [react()],
 })
